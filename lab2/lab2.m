ebf269a79f841cd214241a7cc3a85943da1d1760
Gaurangi Siriya <s.gaurangi12@gmail.com>
2020-10-29 11:40:07 -0400

    finished up to output 3 -- need to verify outputs tho

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -5,16 +5,16 @@ span_V = [1 -1 0 1; 1 1 0 0; 3 1 0 1]';
 span_W = [1 0 2 1; 1 0 -2 0]';
 
 %find the basis for both v and w respectively by using the built-in matlab
-%function orth
+%function orth (output 1)
 V = orth(span_V);
 W = orth(span_W);
 
 %call function sub_sum to generate the sum of the two defined subspaces
-sum_VW = sub_sum(V,W);
+sum_VW = sub_sum(V,W)
 
 %call function sub_intersect to find the intersection for the two defined
-%subspaces 
-int_VW = sub_intersect(V,W);
+%subspaces (output 2 is line 13 and 17)
+int_VW = sub_intersect(V,W)
 
 %---Part 3.2: Linear Transformations and Linear Equations--- 
 
@@ -28,7 +28,9 @@ x = [2;1];
 P = [x1, x2];
 
 p_inv = inv(P);
-z = p_inv * x 
+z = p_inv * x; 
 
 %numerically verify that x can be written as an l.c of x1 and x2 and z
+%(output 3)
 x_verified = z(1)*x1 + z(2)*x2; 
+
--- lab2/sub_intersect.m
+++ lab2/sub_intersect.m
@@ -1,8 +1,10 @@
 function D = sub_intersect(A,B)
 %should the output just be the nullspace of the matrices? tbd  
 x = [A -B];
-D = null(x);
+n = null(x);
 
 %we probably need to find the set of vectors that satisfy that line x = l*v
-%+ u*w given the nullspace we found
+%, u*w given the nullspace we found
+lambda = [n(1);n(2)];
+D = A*lambda; 
 end 

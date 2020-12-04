911d2d79b8bcd9a22036233af0a17b40420a15a1
Isamu <isamu.poy@mail.utoronto.ca>
2020-12-04 17:11:40 +0900

    graphed some graphs for 3.2

--- lab4/dynamics.m
+++ lab4/dynamics.m
@@ -1,3 +1,18 @@
-function x = dynamics(t,ic,parameters,K,A,B,C,L) %t-time;x-state;parameters-M,g,l
+function dyn = dynamics(t, ic,K,A,B,C,L) %t-time;x-state;parameters-M,g,l
     % TODO: return a vector containing the state feedback and estimator
-    % x = [xdot; xest]
+    %extract pendulum states
+    x1 = ic(1)
+    x2 = ic(2)
+    x3 = ic(3)
+    x4 = ic(4)
+    
+    x = [x1 x2 x3 x4];
+    
+    y = C*x'
+    xdot = (A+B*K)*x';
+    xdotHat = (A+L*C)*x' + B*K*x' + L*y;
+    
+    dyn = [xdot; xdotHat]
+    %dyn = dyn(:)
+    
+end
--- lab4/lab4.m
+++ lab4/lab4.m
@@ -11,7 +11,8 @@ xbar = [0 0 0 0]';
 
 % define symbolic variables
 syms x1 x2 x3 x4 u M m l g;
-
+x_set = [x1;x2;x3;x4];
+x_set2 = [x1; x3];
 % define xdot 
 xdot = [x2; ((-m.*l.*sin(x3).*x4.*x4)+(m*g*sin(x3)*cos(x3)+u))./(M+m.*sin(x3).*sin(x3));
         x4; ((-m*l.*sin(x3).*cos(x3).*x4.*x4)+(M+m).*g.*sin(x3)+u.*cos(x3))./(l.*(M+m.*sin(x3).*sin(x3)))];
@@ -56,15 +57,150 @@ obsv(A,C) % observability matrix rank is 4, therefore observable
 % calculate K matrices, followed by L based on the rule that L = K'
 % Remember that Matlab uses the convention that u = −Kx
 K = place(A', C', eval1)
-L1 = K';
+L1 = -K';
 K = place(A', C', eval2)
-L2 = K';
+L2 = -K';
 
 % calculate K for state feedback controller
 p = [-5.1 -5.2 -5.3 -5.4]
-K = place(A, B, p)
+K = -place(A, B, p)
+
 
 % Simulate this system using ic: (−0.5, 0, −π/4, 0) and (0, 0, 0, 0) for x
 % TODO: create a function expressing the dynamics: feedback and estimator
-ic1 = [-0.5, 0, -pi/4, 0];
-ic2 = [0,0,0,0];
+x1 = [-0.5;0;-pi/4;0;0;0;0;0];
+
+Tspan = linspace(0,10,1e3);
+options = odeset('RelTol', 1e-7,'AbsTol', 1e-7);
+
+% define the linear dynamics
+lin = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L1*C) * x(5:8) + B*K*x(1:4) - L1*C * x(1:4)];
+lin2 = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L2*C) * x(5:8) + B*K*x(1:4) - L2*C * x(1:4)];
+
+[t,x]=ode45(lin,Tspan,x1, options);
+[t,x2]=ode45(lin2,Tspan,x1, options);
+
+xdelta = x(:,5:8) - x(:,1:4);
+xdelta2 = x2(:,5:8) - x2(:,1:4);
+
+figure('Name', 'decoupled linear states');
+subplot(2,2,1);
+plot(t, xdelta(:,1));
+hold on; 
+plot(t, xdelta2(:,1));
+title('i = 1');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,2);
+plot(t, xdelta(:,2));
+hold on; 
+plot(t, xdelta2(:,2));
+title('i = 2');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,3);
+plot(t, xdelta(:,3));
+hold on; 
+plot(t, xdelta2(:,3));
+title('i = 3');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,4);
+plot(t, xdelta(:,4));
+hold on; 
+plot(t, xdelta2(:,4));
+
+title('i = 4');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+syms x1 x2 x3 x4;
+
+M = parameters.M;
+m = parameters.m;
+l = parameters.l;
+g = parameters.g;
+
+xdot = [x2; ((-m.*l.*sin(x3).*x4.*x4)+(m*g*sin(x3)*cos(x3)+u))./(M+m.*sin(x3).*sin(x3));
+        x4; ((-m*l.*sin(x3).*cos(x3).*x4.*x4)+(M+m).*g.*sin(x3)+u.*cos(x3))./(l.*(M+m.*sin(x3).*sin(x3)))];
+% define the nonlinear dynamics% something wrong with xdot
+
+lin = @(t,x) [x(2); ((-m.*l.*sin(x(3)).*x(4).*x(4))+(m*g*sin(x(3))*cos(x(3))+K*x(1:4)))./(M+m.*sin(x(3)).*sin(x(3)));
+                x(4); ((-m*l.*sin(x(3)).*cos(x(3)).*x(4).*x(4))+(M+m).*g.*sin(x(3))+K*x(1:4).*cos(x(3)))./(l.*(M+m.*sin(x(3)).*sin(x(3))));
+                    (A + L1*C) * x(5:8) + B*K*x(1:4) - L1*[x(1);x(3)]];
+                
+lin2 = @(t,x) [x(2); ((-m.*l.*sin(x(3)).*x(4).*x(4))+(m*g*sin(x(3))*cos(x(3))+K*x(1:4)))./(M+m.*sin(x(3)).*sin(x(3)));
+                x(4); ((-m*l.*sin(x(3)).*cos(x(3)).*x(4).*x(4))+(M+m).*g.*sin(x(3))+K*x(1:4).*cos(x(3)))./(l.*(M+m.*sin(x(3)).*sin(x(3))));
+                    (A + L2*C) * x(5:8) + B*K*x(1:4) - L2*[x(1);x(3)]];
+
+
+x_i = [-0.5;0;-pi/4;0;0;0;0;0];
+[t,x]=ode45(lin,Tspan,x_i, options);
+[t,x2]=ode45(lin2,Tspan,x_i, options);
+
+xdelta = x(:,5:8) - x(:,1:4);
+xdelta2 = x2(:,5:8) - x2(:,1:4);
+
+figure('Name', 'decoupled nonlinear states');
+subplot(2,2,1);
+plot(t, xdelta(:,1));
+hold on; 
+plot(t, xdelta2(:,1));
+title('i = 1');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,2);
+plot(t, xdelta(:,2));
+hold on; 
+plot(t, xdelta2(:,2));
+title('i = 2');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,3);
+plot(t, xdelta(:,3));
+hold on; 
+plot(t, xdelta2(:,3));
+title('i = 3');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,4);
+plot(t, xdelta(:,4));
+hold on; 
+plot(t, xdelta2(:,4));
+
+title('i = 4');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+% ================= 3.2 State Estimation with Measurement Noise ======================
+% Using the Matlab randn function, create an array of 1000 2 × 1 random vectors with mean and covariance
+
+% Create an anonymous function W(t) interpolating this array over your integration interval.
+
+% Create new functions simulating the state feedback control system plus the observer
+
+% Simulate the system as before, using both L1 and L2 and produce one figure showing the estimation error
+
+% Calculate the mean squared estimation error (MSE) over the final 5 seconds of each simulation.

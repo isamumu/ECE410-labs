6477b9424d48a1d66c3689bd7482ed28d796845b
Isamu <isamu.poy@mail.utoronto.ca>
2020-12-04 22:28:17 +0900

    simulated noise. Going to check with Bryan.

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -77,12 +77,15 @@ options = odeset('RelTol', 1e-7,'AbsTol', 1e-7);
 lin = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L1*C) * x(5:8) + B*K*x(1:4) - L1*C * x(1:4)];
 lin2 = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L2*C) * x(5:8) + B*K*x(1:4) - L2*C * x(1:4)];
 
+% integrate the linear dynamics
 [t,x]=ode45(lin,Tspan,x1, options);
 [t,x2]=ode45(lin2,Tspan,x1, options);
 
+% calculate error states
 xdelta = x(:,5:8) - x(:,1:4);
 xdelta2 = x2(:,5:8) - x2(:,1:4);
 
+% plot the linear dynamics
 figure('Name', 'decoupled linear states');
 subplot(2,2,1);
 plot(t, xdelta(:,1));
@@ -125,17 +128,13 @@ ylabel('position');
 hold on;
 legend('L1', 'L2');
 
-syms x1 x2 x3 x4;
-
+% part 2 of plotting
 M = parameters.M;
 m = parameters.m;
 l = parameters.l;
 g = parameters.g;
 
-xdot = [x2; ((-m.*l.*sin(x3).*x4.*x4)+(m*g*sin(x3)*cos(x3)+u))./(M+m.*sin(x3).*sin(x3));
-        x4; ((-m*l.*sin(x3).*cos(x3).*x4.*x4)+(M+m).*g.*sin(x3)+u.*cos(x3))./(l.*(M+m.*sin(x3).*sin(x3)))];
-% define the nonlinear dynamics% something wrong with xdot
-
+% define the nonlinear dynamics% 
 lin = @(t,x) [x(2); ((-m.*l.*sin(x(3)).*x(4).*x(4))+(m*g*sin(x(3))*cos(x(3))+K*x(1:4)))./(M+m.*sin(x(3)).*sin(x(3)));
                 x(4); ((-m*l.*sin(x(3)).*cos(x(3)).*x(4).*x(4))+(M+m).*g.*sin(x(3))+K*x(1:4).*cos(x(3)))./(l.*(M+m.*sin(x(3)).*sin(x(3))));
                     (A + L1*C) * x(5:8) + B*K*x(1:4) - L1*[x(1);x(3)]];
@@ -144,14 +143,18 @@ lin2 = @(t,x) [x(2); ((-m.*l.*sin(x(3)).*x(4).*x(4))+(m*g*sin(x(3))*cos(x(3))+K*
                 x(4); ((-m*l.*sin(x(3)).*cos(x(3)).*x(4).*x(4))+(M+m).*g.*sin(x(3))+K*x(1:4).*cos(x(3)))./(l.*(M+m.*sin(x(3)).*sin(x(3))));
                     (A + L2*C) * x(5:8) + B*K*x(1:4) - L2*[x(1);x(3)]];
 
-
+% define initial conditions
 x_i = [-0.5;0;-pi/4;0;0;0;0;0];
+
+% integrate the nonlinear dynamics
 [t,x]=ode45(lin,Tspan,x_i, options);
 [t,x2]=ode45(lin2,Tspan,x_i, options);
 
+% find the error states
 xdelta = x(:,5:8) - x(:,1:4);
 xdelta2 = x2(:,5:8) - x2(:,1:4);
 
+% plot the nonlinear dynamics
 figure('Name', 'decoupled nonlinear states');
 subplot(2,2,1);
 plot(t, xdelta(:,1));
@@ -196,11 +199,104 @@ legend('L1', 'L2');
 
 % ================= 3.2 State Estimation with Measurement Noise ======================
 % Using the Matlab randn function, create an array of 1000 2 × 1 random vectors with mean and covariance
+randVec = randn(2,1000); % normally distributed random vectors of zero mean and unit variance
+mean = [0;0];
+cov = [0.005 0; 0 0.001];
+L = sqrt(cov); % by definition of the Cholesky decomposition
+x_rand = L*randVec;
 
 % Create an anonymous function W(t) interpolating this array over your integration interval.
+W = @(t,x)interp1(Tspan, x_rand', t);
 
 % Create new functions simulating the state feedback control system plus the observer
+lin = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L1*C) * x(5:8) + B*K*x(1:4) - L1*C*x(1:4) + L1*W(t)'];
+lin2 = @(t,x) [A*x(1:4) + B*K*x(1:4); (A + L2*C) * x(5:8) + B*K*x(1:4) - L2*C*x(1:4) + L2*W(t)'];
 
 % Simulate the system as before, using both L1 and L2 and produce one figure showing the estimation error
+x_i = [-0.5;0;-pi/4;0;0;0;0;0];
+
+% integrate the new feedback control system
+[t,x]=ode45(lin,Tspan,x_i, options);
+[t,x2]=ode45(lin2,Tspan,x_i, options);
+
+figure('Name', 'decoupled nonlinear states');
+subplot(2,2,1);
+plot(t, xdelta(:,1));
+hold on; 
+plot(t, xdelta2(:,1));
+title('i = 1');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,2);
+plot(t, xdelta(:,2));
+hold on; 
+plot(t, xdelta2(:,2));
+title('i = 2');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,3);
+plot(t, xdelta(:,3));
+hold on; 
+plot(t, xdelta2(:,3));
+title('i = 3');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
+
+subplot(2,2,4);
+plot(t, xdelta(:,4));
+hold on; 
+plot(t, xdelta2(:,4));
+
+title('i = 4');
+xlabel('t');
+ylabel('position');
+hold on;
+legend('L1', 'L2');
 
 % Calculate the mean squared estimation error (MSE) over the final 5 seconds of each simulation.
+% obtain the error states
+xtilda = x(:,5:8) - x(:,1:4);
+xtilda2 = x2(:,5:8) - x2(:,1:4);
+
+N = 1000; % in total 1000 iterations aka 10 seconds
+
+% calculate the MSEs (start from 5 seconds aka iteration # 500)
+% state i = 1
+MSE1 = 0;
+for k = 500:1:N
+    MSE1 = MSE1 + xtilda(k,1) * xtilda(k,1);
+end
+MSE1 = (1/N) * MSE1
+
+% state i = 2
+MSE2 = 0;
+for k = 500:1:N
+    MSE2 = MSE2 + xtilda(k,2) * xtilda(k,2);
+end
+MSE2 = (1/N) * MSE2
+
+% state i = 3
+MSE3 = 0;
+for k = 500:1:N
+    MSE3 = MSE3 + xtilda(k,3) * xtilda(k,3);
+end
+MSE3 = (1/N) * MSE3
+
+% state i = 4
+MSE4 = 0;
+for k = 500:1:N
+    MSE4 = MSE4 + xtilda(k,4) * xtilda(k,4);
+end
+MSE4 = (1/N) * MSE4
+
+% ================= 4.1 Noiseless Output Feedback Control ======================
+%TODO
+
